3ba36de1bf4777657f28b023088f2cee1b4afddd
John Albert <jpalbert81@gmail.com>
2022-11-01 01:05:19 -0700

    incorporating web api

--- Ror2Hud.cs
+++ Ror2Hud.cs
@@ -12,18 +12,18 @@ namespace com.thejpaproject.ror2hud
 {
 
   [BepInDependency(R2API.R2API.PluginGUID)]
-
   [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
   public class Ror2Hud : BaseUnityPlugin
   {
     private static protected ManualLogSource _logger;
     public const string PluginGUID = PluginAuthor + "." + PluginName;
     public const string PluginAuthor = "com.thejpaproject";
-    public const string PluginName = "ror2hud";
+    public const string PluginName = "Ror2Hud";
     public const string PluginVersion = "1.0.2";
 
+    private bool timeDataLoaded = false;
     private float timer = 0f;
-    private float delay = .5f;
+    private float delay = 1f;
 
     public void Awake()
     {
@@ -32,21 +32,56 @@ namespace com.thejpaproject.ror2hud
       On.RoR2.UI.HUD.Awake += MyHud;
       On.RoR2.UI.HUD.Update += Update;
 
-      _logger.LogInfo("loaded");
+      On.RoR2.UI.MainMenu.BaseMainMenuScreen.OnEnter += OnEnter;
+
+      _logger.LogInfo("Loaded");
+
+      RaceApiTimeData myTest = new RaceApiTimeData();
+      myTest.statusCode = "444";
+      myTest.body = new Body();
+      myTest.body.item = new Item();
+      myTest.body.item.endTime = 1666929600000;
+      myTest.body.item.startTime = 1666911600000;
+      myTest.body.item.ID = "Times";
+      myTest.body.item.timeInc = 18000000;
+
+      _logger.LogInfo(myTest + " :  " + JsonUtility.ToJson(myTest.body.item, true));
+
+      _logger.LogInfo("body ?" + myTest.body);
+      _logger.LogInfo("body.item ?" + myTest.body.item);
+      _logger.LogInfo(myTest + " :  " + JsonUtility.ToJson(myTest.body, true));
+
+
+
     }
+
+
+    void OnEnter(On.RoR2.UI.MainMenu.BaseMainMenuScreen.orig_OnEnter orig, RoR2.UI.MainMenu.BaseMainMenuScreen self, RoR2.UI.MainMenu.MainMenuController mainMenuController)
+    {
+      orig(self, mainMenuController);
+      if (!timeDataLoaded)
+      {
+        RacesApi.LoadTimeData(self);
+        timeDataLoaded = true;
+      }
+    }
+
 
     private void Update(On.RoR2.UI.HUD.orig_Update orig, RoR2.UI.HUD self)
     {
+      orig(self);
 
       timer += Time.deltaTime;
 
       if (timer > delay)
       {
-        orig(self);
         var children = self.GetComponentsInChildren<Text>();
         foreach (Text t in children)
         {
           _logger.LogInfo(DateTime.Now);
+          _logger.LogInfo("Time Data ? " + RacesApi.TimeData.body);
+          _logger.LogInfo("Time Data ? " + RacesApi.TimeData.body?.item?.endTime);
+
           t.text = DateTime.Now.ToString();
         }
         timer = timer - delay;
@@ -93,6 +128,7 @@ namespace com.thejpaproject.ror2hud
     // {
     //   On.RoR2.UI.HUD.Awake -= MyHud;
     // }
+
 
 
   }
